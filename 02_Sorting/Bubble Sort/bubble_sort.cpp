5f284827640e74a581a275c6e7a75256659de0b6
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-07-25 22:03:53 +0530

    add recursive bubble sort

--- 02_Sorting/Bubble Sort/bubble_sort.cpp
+++ 02_Sorting/Bubble Sort/bubble_sort.cpp
@@ -2,6 +2,52 @@
 using namespace std;
 
 void bubble_sort(int arr[], int size);
+void bubbleSwap(int arr[], int start, int stop){
+  // base condition
+  if(start > stop)
+    return;
+
+  // now, after, say, stop = 10
+  // start = 9
+  // do something
+  // call bubbleSwap -> burst finish
+
+  if (arr[start] > arr[stop]){
+    int temp = arr[start];
+    arr[start] = arr[stop];
+    arr[stop] =  temp;
+
+
+    cout << "(" << arr[stop] << "," << arr[start] << ")"
+         << "(" << arr[start] << "," << arr[stop] << ")" << endl;
+  }
+  
+  bubbleSwap(arr, start+1, stop);
+
+
+}
+void bubble_sort_recursion(int arr[], int start, int stop){
+  // fn01 -> no of iterate
+  // fn02 -> iterate through array & swap
+
+  // for fn01: base case -> n <= 0
+  // for fn02: base case -> n <= 1
+
+  // fn01 -> @params -> (arr[], start, stop)
+  // fn02 -> @params -> (arr[], start, stop)
+
+  // fn02 -> bubbleSwap
+
+  if(start > stop)
+    return;
+
+  bubbleSwap(arr, start, stop-start);
+
+
+
+  bubble_sort_recursion(arr, start+1, stop);
+  
+}
 void getArray(int arr[], int size);
 void showArray(int arr[], int size);
 void swap(int &x, int &y);
@@ -14,7 +60,8 @@ int main(int argc, char* argv[]){
 
   getArray(arr, size);
 
-  bubble_sort(arr, size);
+  // bubble_sort(arr, size);
+  bubble_sort_recursion(arr, 0, size-1);
 
   showArray(arr, size);
 return 0;
--- 02_Sorting/Bubble Sort/bubble_sort.py
+++ 02_Sorting/Bubble Sort/bubble_sort.py
@@ -5,10 +5,30 @@ def bubble_sort(arr):
         arr[j], arr[j+1] = arr[j+1], arr[j]
   return
 
+def bubble_swap(arr, start, stop):
+  if start >= stop:
+    return
+  
+  if arr[start] > arr[stop]:
+    arr[start], arr[stop] = arr[stop], arr[start]
+  
+  bubble_swap(arr, start+1, stop)
+  return
+
+def bubble_sort_recursion(arr, start, stop):
+  if start >= stop:
+    return
+  
+  bubble_swap(arr, start, stop-start)
+
+  bubble_sort_recursion(arr, start+1, stop)
+  return
+
 size = int(input("size: "));
 
 arr = list(map(int, input("Enter elements of array: ").strip().split()[:size]))
 
-bubble_sort(arr)
+# bubble_sort(arr)
+bubble_sort_recursion(arr, 0, size-1)
 
 print(arr)
