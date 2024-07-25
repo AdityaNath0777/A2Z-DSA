e087462e667c4add9413140a720b664a58a07116
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-07-25 22:30:27 +0530

    add recursive selection sort

--- 02_Sorting/Selection Sort/selection_sort.py
+++ 02_Sorting/Selection Sort/selection_sort.py
@@ -10,10 +10,36 @@ def selection_sort(arr, size):
       arr[min_index], arr[i] = arr[i], arr[min_index]
   return
 
+def select_min_index(arr, start, stop, min_index):
+  # base condition
+  if start > stop:
+    return min_index
+  
+  if arr[min_index] > arr[start]:
+    min_index = start
+
+  min_index = select_min_index(arr, start+1, stop, min_index)
+  return min_index
+
+def selection_sort_recursion(arr, start, stop):
+  # base condition
+  if start == stop:
+    return
+  
+  min_index = start
+  min_index = select_min_index(arr, start+1, stop, min_index)
+  if(min_index != start):
+    arr[min_index], arr[start] = arr[start], arr[min_index]
+    
+  selection_sort_recursion(arr, start+1, stop)
+  return
+
+
 size = int(input("size: "));
 
 arr = list(map(int, input("Enter elements of array: ").strip().split()[:size]))
 
-selection_sort(arr, size)
+# selection_sort(arr, size)
+selection_sort_recursion(arr, 0, size-1)
 
 print(arr)
