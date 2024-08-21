f397ce8506ede467802cf868fd3910fa6f40dd0c
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-08-22 00:32:20 +0530

    add: longest sequence better in C++

--- 03_Arrays/Medium/C++/10_longest_sequence.cpp
+++ 03_Arrays/Medium/C++/10_longest_sequence.cpp
@@ -9,6 +9,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// TC -> O(N)
 bool linearSearch(vector<int>& v, int num){
   int n = v.size();
 
@@ -24,11 +25,13 @@ int longestSuccessiveElements_brute(vector<int>& nums){
   int longest = 1;
   int n = nums.size();
 
-  for(int i=0; i<n; ++i){
+  for(int i=0; i<n; ++i){ // TC -> O(N)
     int num = nums[i];
     int cnt = 1;
 
     // search using linear search
+    // TC -> O(N)*O(N)
+    //    ~ O(N^2)
     while(linearSearch(nums, num+1)){
       num += 1;
       cnt++;
@@ -36,6 +39,39 @@ int longestSuccessiveElements_brute(vector<int>& nums){
 
     longest = max(longest, cnt);
   }
+
+  // total TC -> O(N^3)
+  // total SC -> O(1)
+  return longest;
+}
+
+
+int longestSuccessiveElements_better(vector<int>& nums){
+  int n = nums.size();
+
+  if(!n) return 0; // 0 length -> empty array/vector
+
+  // sort the array/vector 
+  sort(nums.begin(), nums.end());
+
+  int prev = INT_MIN;
+  int cnt = 0;
+  int longest = 1;
+
+
+  for(int i=0; i<n; ++i){
+    if(nums[i] - 1 == prev){
+      cnt++;
+      prev = nums[i];
+    }
+    else if(nums[i] != prev){
+      cnt = 0;
+      prev = nums[i];
+    }
+
+    longest = max(longest, cnt);
+  }
+
   return longest;
 }
 
@@ -51,7 +87,9 @@ int main(int argc, char* argv[]){
     v.push_back(a);
   }
 
-  int maxSeqLength = longestSuccessiveElements_brute(v);
+  int maxSeqLength;
+  // maxSeqLength = longestSuccessiveElements_brute(v);
+  maxSeqLength = longestSuccessiveElements_better(v);
 
   cout << maxSeqLength << endl;
 return 0;
