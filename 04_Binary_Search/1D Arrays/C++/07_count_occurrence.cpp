d6268ca35e44b8c334e767685e00ec0d0f143b9e
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-08-25 19:49:22 +0530

    update: add count occurrences optimal in C++

--- 04_Binary_Search/1D Arrays/C++/07_count_occurrence.cpp
+++ 04_Binary_Search/1D Arrays/C++/07_count_occurrence.cpp
@@ -1,23 +1,153 @@
+/*
+ * Problem Statement
+ * 
+ * You are given a sorted array containing N integers and a number X, 
+ * you have to find the occurrences of X in the given array.
+ */
 
 #include<bits/stdc++.h>
 using namespace std;
-int count_brute(vector<int>& arr, int n, int x) {
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
 
-        // counting the occurrences:
-        if (arr[i] == x) cnt++;
-    }
-    return cnt;
+int count_brute(vector<int>& arr, int n, const int &target) {
+  int cnt = 0;
+
+  for(int i=0; i<n; ++i){
+    if(arr[i] == target)
+      cnt++;
+  }
+
+  return cnt;
 }
 
-int main()
-{
-    vector<int> arr =  {2, 4, 6, 8, 8, 8, 11, 13};
-    int n = 8, x = 8;
-    int ans = count_brute(arr, n, x);
-    cout << "The number of occurrences is: "
-         << ans << "\n";
+int firstOccurrence(vector<int>& arr, const int &target){
+  int n = arr.size();
+  int first = -1;
+
+  int low = 0;
+  int high = n-1;
+
+  while(low <= high){
+    int mid = low + (high-low)/2;
+
+    if(arr[mid] == target){
+      first = mid;
+      high = mid - 1;
+    }
+    else if(arr[mid] < target){
+      low = mid + 1;
+    }
+    else {
+      high = mid - 1;
+    }
+  }
+
+  return first;
+}
+
+int lastOccurrence(vector<int>& arr, const int &target){
+  int n = arr.size();
+  int last = -1;
+
+  int low = 0;
+  int high = n-1;
+
+  while(low <= high){
+    int mid = low + (high - low)/2;
+
+    if(arr[mid] == target){
+      last = mid;
+      low = mid + 1;
+    }
+    else if(arr[mid] < target){
+      low = mid + 1;
+    }
+    else {
+      high = mid - 1;
+    }
+  }
+
+  return last;
+}
+
+int count_optimal(vector<int>& arr, int n, const int &target) {
+  
+  int cnt = 0;
+  /*
+    n -> 12
+    arr[] = [1 2 2 2 2 4 4 5 5 6 7 7]
+    target -> 2
+
+    we know that array is -> sorted,
+
+    if we take any random value, say num, from the array 
+
+    if num == target
+      cnt++
+
+    if num < target
+      -> it would mean, the target might exist after the num,
+      -> to reach there, we need to move right
+      -> for that -> low = curr_pos + 1
+
+    if num < target
+      -> it would mean, the target might exist before the num,
+      -> to reach there, we need to move left
+      -> for that -> high = curr_pos - 1
+
+
+    We can also do something,
+    if we can somehow know the starting & ending point of the target,
+    then by subtracting the position distance
+    we can get the count
+
+    starting point -> lower bound of target
+    ending point -> upper bound of target
+  */
+
+  int start = firstOccurrence(arr, target);
+
+  // target not found
+  if (start == -1)
     return 0;
+  
+  int end = lastOccurrence(arr, target);
+
+  // adding 1 to include the 'start' and 'end' indices
+  cnt = end - start + 1;
+
+  return cnt;
+}
+
+int count_optimal_STL(vector<int>& arr, int n, const int &target) {
+
+  // if we use C++ STL
+  auto first = lower_bound(arr.begin(), arr.end(), target);
+  auto last = upper_bound(arr.begin(), arr.end(), target);
+
+  return int(last - first);
+}
+
+int main(){
+  vector<int> v;
+
+  int size; 
+  cin>>size;
+
+  int target;
+  cin >> target;
+
+  for(int i=0; i<size; ++i){
+    int a;
+    cin >> a;
+    v.push_back(a);
+  }
+
+  int ans;
+  // ans = count_brute(v, size, target);
+  // ans = count_optimal(v, size, target);
+  ans = count_optimal_STL(v, size, target);
+  cout << "The number of occurrences of " << target << " is: "
+        << ans << "\n";
+  return 0;
 }
 
