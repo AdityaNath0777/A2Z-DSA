7f78ff307775b9a0ee077ca11cbdc659ef265321
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-09-03 19:23:20 +0530

    update: add kth missing integer optimal in C++

--- 04_Binary_Search/On Asnwers/C++/07_kth_positive_integer.cpp
+++ 04_Binary_Search/On Asnwers/C++/07_kth_positive_integer.cpp
@@ -88,6 +88,75 @@ int find_kth_missing_integer_better(vector<int>& nums, int k) {
     return k;
 }
 
+// optimal -> using Binary Search
+int find_kth_missing_integer_optimal(vector<int>& nums, int k){
+  int n = nums.size();
+  int low = 0;
+  int high = n-1;
+
+  /**
+   * 
+   * basically what we're doing is,
+   * 
+   * after observing the array, it's index and the number it should have 
+   *        to find the kth missing number
+   * 
+   * we got to know, 
+   *    * the array must contain n+k elements
+   *    * the difference b/w two consecutive elements would be 1 is no number is missing b/w them
+   *    * ideally -> (index + 1) should be the element at that index
+   *   
+   * so we're subtracting the element at index mid with the number it's supposed to contain
+   * i.e. nums[mid] - (mid + 1)
+   * 
+   * if diff > k:
+   *    upto the current index there are more missing numbers than the required one
+   *    eliminate right -> move left
+   *    high = mid - 1;
+   * 
+   * if diff == k:
+   *    upto the current index, there are required missing numbers,
+   *    However, we need to continue searching to find the smallest index where this happens.
+   *    to ensures we accurately determine the k-th missing number's position.
+   *    eliminate right -> move left
+   *    high = mid - 1;
+   * 
+   * if diff < k:
+   *    upto the current index, there are less missing number than the required one
+   *    eliminate left -> move right
+   *    low = mid + 1
+   * 
+   * loop ends
+   * 
+   * We don't break the loop on exact matches because we want to find the first position
+   * where the count of missing numbers is greater than or equal to k.
+   * 
+  */
+  while(low <= high){
+    int mid = low + (high - low)/2;
+
+    int nofMissingNums = nums[mid] - (mid + 1);
+
+    // cout << nofMissingNums << ", \t";
+    // cout << high << " :: " << low << "\n\n";
+    if (nofMissingNums < k) low = mid + 1;
+    else high = mid - 1;;
+
+  }
+
+  // cout << endl;
+  // cout << high << " :: " << low << "\n\n";
+
+  // After the loop, low is the position where the k-th missing number would be.
+  // The k-th missing number is k more than the number of elements before it.
+  return k + low;
+
+  // or
+  // return k + high + 1;
+  // as the loop ends when low crosses high, 
+  //    and they end up adjacent to each other, differing by 1
+}
+
 int main(int argc, char* argv[]){
   vector<int> v;
 
@@ -107,7 +176,8 @@ int main(int argc, char* argv[]){
   int kthMissingNum;
 
   // kthMissingNum = find_kth_missing_integer_my_brute(v, k);
-  kthMissingNum = find_kth_missing_integer_better(v, k);
+  // kthMissingNum = find_kth_missing_integer_better(v, k);
+  kthMissingNum = find_kth_missing_integer_optimal(v, k);
 
   cout << "kth missing positive integer is: " << kthMissingNum;
 
