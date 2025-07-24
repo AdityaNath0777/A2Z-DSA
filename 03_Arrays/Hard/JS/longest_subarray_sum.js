d19050cf4ec84bea3545fe3f2e9b410f774ac28e
Aditya_Mishra <adityasaiyansuper@gmail.com>
2025-07-24 22:02:38 +0530

    update: implement optimal longest subarray sum in js

--- 03_Arrays/Hard/JS/longest_subarray_sum.js
+++ 03_Arrays/Hard/JS/longest_subarray_sum.js
@@ -10,13 +10,14 @@
  *
  *    Explanation: The following subarrays sum to zero:
  *    {-3, 3} , {-1, 6, -5}, {-3, 3, -1, 6, -5}
- *    Since we require the length of the longest subarray, our answer is 5!
+ *    Since we require the length of the longest subarray,
+ *    our answer is 5!
  */
 
 function longestSubarraySum_Brute(nums = [], target = 0) {
   let n = nums.length;
 
-  let maxLen = -1;
+  let maxLen = 0;
 
   for (let i = 0; i < n; ++i) {
     for (let j = i; j < n; ++j) {
@@ -46,14 +47,14 @@ function longestSubarraySum_Brute(nums = [], target = 0) {
 function longestSubarraySum_Better(nums = [], target = 0) {
   let n = nums.length;
 
-  let maxLen = -1;
+  let maxLen = 0;
 
   for (let i = 0; i < n; ++i) {
     let currentSum = 0; // reset for each new starting point
     for (let j = i; j < n; ++j) {
       currentSum += nums[j]; // accumulating the sum
 
-      if (currentSum == 0) {
+      if (currentSum === target) {
         let len = j - i + 1;
         maxLen = Math.max(maxLen, len);
       }
@@ -70,6 +71,33 @@ function longestSubarraySum_Better(nums = [], target = 0) {
    */
 }
 
+function longestSubarraySum_Optimal(nums = [], target = 0) {
+  let n = nums.length;
+
+  let maxLen = 0;
+  let currentSum = 0;
+  const sumMap = new Map();
+
+  for (let i = 0; i < n; i++) {
+    currentSum += nums[i];
+
+    let complement = currentSum - target;
+
+    if (sumMap.has(complement)) {
+      // if we got the complement means at from current index 
+      // to that index we have the required total summation target
+
+      maxLen = Math.max(maxLen, i - sumMap.get(complement));
+    }
+
+    if (!sumMap.has(currentSum)) {
+      sumMap.set(currentSum, i);
+    }
+  }
+
+  return maxLen;
+}
+
 const arr = [9, -3, 3, -1, 6, -5];
 
 let ans;
@@ -79,3 +107,6 @@ console.log("brute ans: %d", ans);
 
 ans = longestSubarraySum_Better(arr);
 console.log("better ans: %d", ans);
+
+ans = longestSubarraySum_Optimal(arr);
+console.log("Optimal ans: %d", ans);
